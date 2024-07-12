29df3b5bb13003d9dc292bc6e3be46742a3a8580
yungcxn <can.nayci@gmx.de>
2024-07-12 13:03:15 +0200

    added some camera parameters

--- src/ingame/cam/cam.h
+++ src/ingame/cam/cam.h
@@ -2,12 +2,24 @@
 #define CAM_H
 
 #include "../../util/vec.h"
+#include "../../util/primitive.h"
+
+typedef enum {
+  CAM_NORMAL = 0,
+  CAM_DARKNESS,
+  CAM_FLASH
+} cam_lighting_mode;
 
 /* camera width and height is determined by gfx */
 typedef struct {
   bool is_needed;
   bool fixed;
+  uint32_t shake_ticks_left;
+  byte shake_mod;
+  uint32_t lag_ticks_left;
+  byte lag_mod;
   vec2f pos;
+  
 } cam_t;
 
 #endif
